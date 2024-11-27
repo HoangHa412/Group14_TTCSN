094584bd7aba4f5838d09bd2a036a85a3c319ee0
HoangHa <tunjin123@gmail.com>
2024-11-27 21:19:09 +0700

    upcode

---
+++ src/main/java/com/example/btl_ttcsn_14/BtlTtcsn14Application.java
@@ -0,0 +1,13 @@
+package com.example.btl_ttcsn_14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlTtcsn14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtlTtcsn14Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/btl_ttcsn_14/BtlTtcsn14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btl_ttcsn_14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlTtcsn14ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
